097722ca578ba6c49ac6d3337c78fa7542ebf536
gxlzlihao <gxlzlihao@gmail.com>
2016-03-31 21:12:50 +0800

    Add animation effect to the passedEvents page

--- PassedEvents/passedEvents_main.js
+++ PassedEvents/passedEvents_main.js
@@ -4,6 +4,9 @@
 
 $(document).ready(function(){
 
+    var picture_array = new Array();
+    var target_picture = -1;
+
     // set up the layout of the picture group
     $('div.picture_group').each(function(){
         var image_number = $(this).children('img').length;
@@ -33,34 +36,122 @@ $(document).ready(function(){
 
     // set up animation of the showup area
     var left_click_callback = function(){
-        $(this).animate({'width':'640px', 'height':'400px', 'top':'0', 'left':'370px', 'opacity':'1', 'z-index':'100'},
+
+        target_picture = target_picture - 1;
+
+        $(this).animate({'width':'640px', 'height':'400px', 'top':'0', 'left':'370px', 'right':'370px', 'opacity':'1', 'z-index':'100'},
             'slow',
             'linear',
             function(){
                 $(this).addClass('showup_focus').removeClass('showup_left');
-                $(this).click(null);
             });
-        $(this).siblings('img.showup_focus').animate({'width':'320px', 'height':'200px', 'top':'100px', 'left':'1060px', 'opacity':'0.7', 'z-index':'50'},
+        $(this).siblings('img.showup_focus').animate({'width':'320px', 'height':'200px', 'top':'100px', 'left':'1060px', 'right':'0', 'opacity':'0.7', 'z-index':'50'},
             'slow',
+            'linear',
+            function(){
+                $(this).addClass('showup_right').removeClass('showup_focus').css({'cursor':'pointer'});
+                $(this).off( 'click' );
+                $(this).one( 'click', right_click_callback );
+            });
+        var new_prepare_left = null;
+        if ( target_picture - 1 >= 0 && $(this).siblings('img.showup_prepare_left').length > 0 &&
+            ( $(this).siblings('img.showup_prepare_left').css('display') == 'none' || $(this).siblings('img.showup_prepare_left').attr('src') == null) ) {
+            $(this).siblings('img.showup_prepare_left').css({'display':'block', 'opacity':'0'});
+            $('img.showup_prepare_left').attr('src', picture_array[target_picture - 1]);
+            $('img.showup_prepare_left').attr('alt', picture_array[target_picture - 1]);
+        }
+        $(this).siblings('img.showup_prepare_left').animate({'z-index': '50', 'opacity': '0.7'},
+            'slow',
+            'linear',
+            function () {
+                $(this).addClass('showup_left').removeClass('showup_prepare_left');
+                new_prepare_left = $('<image class="showup_prepare_left"></image>');
+                if (target_picture - 2 >= 0) {
+                    new_prepare_left.appendTo('div.showup_area').css({'opacity': '0', 'z-index': '25'});
+                    new_prepare_left.css({'display': 'block'});
+                    new_prepare_left.attr('src', picture_array[target_picture - 2]);
+                    new_prepare_left.attr('alt', picture_array[target_picture - 2]);
+                } else {
+                    new_prepare_left.appendTo('div.showup_area').css({'opacity': '0', 'z-index': '25'});
+                    new_prepare_left.css({'display': 'none'});
+                    new_prepare_left.attr('src', null);
+                    new_prepare_left.attr('alt', null);
+                }
+                $(this).off( 'click' );
+                $(this).one( 'click', left_click_callback );
+            });
+
+        $(this).siblings('img.showup_right').animate({'z-index':'25', 'opacity':'0'},
+            'normal',
             'linear',
             function(){
                 $(this).siblings('img.showup_prepare_right').remove();
-                $(this).siblings('img.showup_right').addClass('showup_prepare_right').removeClass('showup_right');
-                $(this).addClass('showup_right').removeClass('showup_focus');
+                $(this).addClass('showup_prepare_right').removeClass('showup_right');
             });
-        var new_prepare_left = $(this).siblings('img.showup_prepare_left').clone();
-        $(this).siblings('img.showup_prepare_left').animate({'z-index':'50', 'opacity':'0.7'},
+
+    };
+    if ( $('img.showup_left').css('display') != 'none' )
+        $('img.showup_left').one( 'click', left_click_callback );
+
+    var right_click_callback = function() {
+
+        target_picture = target_picture + 1;
+
+        $(this).animate({'width':'640px', 'height':'400px', 'top':'0', 'left':'370px', 'right':'370px', 'opacity':'1', 'z-index':'100'},
             'slow',
             'linear',
             function(){
-                new_prepare_left.appendTo('div.showup_area');
-                // TODO: give the new_prepare_left element image source and alternative source
-                $(this).addClass('showup_left').removeClass('showup_prepare_left');
-                $(this).click( left_click_callback );
+                $(this).addClass('showup_focus').removeClass('showup_right');
             });
-        $(this).siblings('img.showup_prepare_right').animate({'z-index':'25', 'opacity':'0'}, 'normal');
-    }
-    $('img.showup_left').click( left_click_callback );
+        $(this).siblings('img.showup_focus').animate({'width':'320px', 'height':'200px', 'top':'100px', 'left':'0', 'right':'1060px', 'opacity':'0.7', 'z-index':'50'},
+            'slow',
+            'linear',
+            function(){
+                $(this).addClass('showup_left').removeClass('showup_focus').css({'cursor':'pointer'});
+                $(this).off( 'click' );
+                $(this).one( 'click', left_click_callback );
+            });
+        $(this).siblings('img.showup_left').animate({'z-index':'25', 'opacity':'0'},
+            'normal',
+            'linear',
+            function(){
+                $(this).siblings('img.showup_prepare_left').remove();
+                $(this).addClass('showup_prepare_left')
+                    .removeClass('showup_left')
+                    .css({'opacity':'0', 'z-index':'25'});
+            });
+        var new_prepare_right = null;
+        if ( target_picture + 1 < picture_array.length && $(this).siblings('img.showup_prepare_right').length > 0 &&
+            ( $(this).siblings('img.showup_prepare_right').css('display') == 'none' || $(this).siblings('img.showup_prepare_right').attr('src') == null) ) {
+            $(this).siblings('img.showup_prepare_right').css({'display':'block', 'opacity':'0'});
+            $('img.showup_prepare_right').attr('src', picture_array[target_picture + 1]);
+            $('img.showup_prepare_right').attr('alt', picture_array[target_picture + 1]);
+        }
+        $(this).siblings('img.showup_prepare_right').animate({'z-index':'50', 'opacity':'0.7'},
+            'slow',
+            'linear',
+            function(){
+                $(this).addClass('showup_right').removeClass('showup_prepare_right');
+                new_prepare_right = $('<image class="showup_prepare_right"></image>');
+                if (target_picture + 2 < picture_array.length) {
+
+                    new_prepare_right.appendTo('div.showup_area').css({'opacity': '0', 'z-index': '25'});
+                    new_prepare_right.css({'display': 'block'});
+                    new_prepare_right.attr('src', picture_array[target_picture + 2]);
+                    new_prepare_right.attr('alt', picture_array[target_picture + 2]);
+                } else {
+                    new_prepare_right.appendTo('div.showup_area').css({'opacity': '0', 'z-index': '25'});
+                    new_prepare_right.css({'display': 'none'});
+                    new_prepare_right.attr('src', null);
+                    new_prepare_right.attr('alt', null);
+                }
+                $(this).off( 'click' );
+                $(this).one( 'click', right_click_callback );
+            });
+
+    };
+    if ( $('img.showup_right').css('display') != 'none' )
+        $('img.showup_right').one( 'click', right_click_callback );
     // end of the animation setup
 
     // set up the hover into and outside animation of the picture
@@ -75,8 +166,94 @@ $(document).ready(function(){
     });
 
     // set up the picture click animation
+    function setup_picture_image( node_name, picture_source ) {
+        $( node_name ).attr( {'src': picture_source} );
+        $( node_name ).attr( {'alt': picture_source} );
+        if ( picture_source == null )
+            $( node_name ).css( {'display':'none'} );
+        else
+            $( node_name ).css( {'display':'block'} );
+    }
+
     $('img.picture').click(function(){
-        //
+
+        picture_array = new Array();
+
+        $(this).parent().children('img.picture').each(function() {
+            picture_array.push( $(this).attr('src') );
+        });
+
+        target_picture = $(this).index();
+
+        // set up the picture source url of the showup area
+        setup_picture_image( 'img.showup_focus', picture_array[ target_picture ] );
+
+        if ( target_picture - 1 >= 0 )
+            setup_picture_image( 'img.showup_left', picture_array[target_picture - 1] );
+        else
+            setup_picture_image( 'img.showup_left', null );
+
+        if ( target_picture - 2 >= 0 )
+            setup_picture_image( 'img.showup_prepare_left', picture_array[target_picture - 2] );
+        else
+            setup_picture_image( 'img.showup_prepare_left', null );
+
+        if ( target_picture + 1 < picture_array.length )
+            setup_picture_image( 'img.showup_right', picture_array[target_picture + 1] );
+        else
+            setup_picture_image( 'img.showup_right', null );
+
+        if ( target_picture + 2 < picture_array.length )
+            setup_picture_image( 'img.showup_prepare_right', picture_array[target_picture + 2] );
+        else
+            setup_picture_image( 'img.showup_prepare_right', null );
+
+        var top_offset = $(this).parent().parent().offset().top + 100;
+        $('div.showup_area').css( {'top': top_offset} );
+        $('div.showup_area').css({'display':'block'}).animate( {'opacity':'1'}, 'slow' );
+
+        $('div#top_picture').animate( {'opacity':'0.25'}, 'slow' );
+        $('div.content').each(function(){
+            $(this).animate( {'opacity':'0.25'}, 'slow' );
+        });
+        var document_height = $(document).height();
+        var document_width = $(document).width();
+        $('div#cover').css({'display':'block', 'width':document_width, 'height':document_height});
+
     });
+
+    var resume_callback = function(){
+
+        $('div.showup_area').animate( {'opacity':'0'}, 'slow', 'linear', function(){ $('div.showup_area').css({'display':'none'}) } );
+        $('div#top_picture').animate( {'opacity':'1'}, 'slow' );
+        $('div.content').each(function(){
+            $(this).animate( {'opacity':'1'}, 'slow' );
+        });
+        $('div#cover').css({'display':'none', 'width':0, 'height':0});
+
+    };
+
+    $('div#cover').click( resume_callback );
+
+    $('div.showup_area').click( function(){
+
+        var click_x = event.pageX;
+        var click_y = event.pageY;
+        var inside_image = false;
+
+        $(this).children('img').each(function(){
+            if ( $(this).css('display') != 'none' ) {
+
+                if ( click_x >= $(this).offset().left && click_x <= ( $(this).offset().left + $(this).width() ) &&
+                        click_y >= $(this).offset().top && click_y <= ( $(this).offset().top + $(this).height() ) )
+                    inside_image = true;
+
+            }
+        });
+
+        if ( inside_image == false )
+            resume_callback();
+
+    } );
 
 });
