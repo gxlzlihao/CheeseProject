74bd622985e8ebd4c6501fa575ab96c68795aa56
gxlzlihao <gxlzlihao@gmail.com>
2016-04-02 18:06:38 +0800

    add programs for introduction fragments panel

--- projectDetails/projectDetails_main.js
+++ projectDetails/projectDetails_main.js
@@ -4,6 +4,20 @@
 
 $(document).ready(function(){
 
-    //
+    $('div#introduction_options').children('div.bar_option').click(function(){
+        $('div#introduction_options').children('div.bar_option').each(function(){
+            $(this).removeClass('active');
+        });
+        $(this).addClass('active');
+
+        // adjustment for the following fragments
+        var index = $(this).index();
+        $('div#introduction_fragments').children('div.fragment').each(function(){
+            if ( $(this).index() == index )
+                $(this).addClass('active');
+            else
+                $(this).removeClass('active');
+        });
+    });
 
 });
