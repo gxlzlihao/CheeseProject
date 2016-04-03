c01fd05e568445183ae4fd815aeaa13ce00c49b1
gxlzlihao <gxlzlihao@gmail.com>
2016-04-03 19:14:06 +0800

    add hover animation to the recommendation sector in project details page

--- projectDetails/projectDetails_main.js
+++ projectDetails/projectDetails_main.js
@@ -22,13 +22,28 @@ $(document).ready(function(){
 
     var image_hover_in_callback = function() {
         // when the mouse move inside the image
-    }
+        $(this).siblings('div.image-cover').stop().css({'display':'block'}).animate({'opacity':'0.8'}, 'slow');
+    };
 
-    var image_hover_out_callback = function() {
+    var image_hover_out_callback = function( event ) {
         // when the mosue move outside the image
-    }
+        var _X = $(this).siblings('div.image-cover').offset().left;
+        var _Y = $(this).siblings('div.image-cover').offset().top;
+        var _width = $(this).siblings('div.image-cover').width();
+        var _height = $(this).siblings('div.image-cover').height();
+        if ( !( event.pageX > _X && event.pageX < _X + _width && event.pageY > _Y && event.pageY < _Y + _height ) )
+            $(this).siblings('div.image-cover').stop().animate({'opacity':'0'}, 'slow', 'linear', function(){
+                $(this).css({'display':'none'});
+            });
+    };
 
-    $('img.background-image').hover( image_hover_in_callback, image_hover_out_callback );
-    $('img.top-image').hover( image_hover_in_callback, image_hover_out_callback );
+    $('img.background-image').mouseover( image_hover_in_callback );
+    $('img.background-image').mouseout( image_hover_out_callback );
+    $('img.top-image').mouseover( image_hover_in_callback );
+    $('img.top-image').mouseout( image_hover_out_callback );
+    $('div.recommendation_item').children('span').mouseover( image_hover_in_callback );
+    $('div.recommendation_item').children('span').mouseout( image_hover_out_callback );
+    $('div.image-cover').mouseover( null );
+    $('div.image-cover').mouseout( null );
 
 });
