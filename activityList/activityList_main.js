508527a448be31714160d53919fcd171121da9df
gxlzlihao <gxlzlihao@gmail.com>
2016-04-10 13:29:48 +0800

    done with activity list page

--- activityList/activityList_main.js
+++ activityList/activityList_main.js
@@ -1,3 +1,15 @@
 /**
  * Created by gxlzlihao on 16-4-8.
  */
+
+$(document).ready(function(){
+
+    var colors = [ '#f25555', 'orange', 'blue', 'green'];
+
+    $('div.item').each(function(){
+        var iter = $(this).index();
+        // console.log( iter + ' -- test' );
+        $(this).children('div.right').children('div.seperation_bar').css({'background-color':colors[ iter % colors.length ]});
+    });
+
+});
