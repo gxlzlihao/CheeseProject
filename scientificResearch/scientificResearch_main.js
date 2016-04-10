f96ee91001448640684a2cc34fc259e13669b453
gxlzlihao <gxlzlihao@gmail.com>
2016-04-10 16:05:26 +0800

    done with scientific research page

--- scientificResearch/scientificResearch_main.js
+++ scientificResearch/scientificResearch_main.js
@@ -1,3 +1,21 @@
 /**
  * Created by gxlzlihao on 16-4-8.
  */
+
+$(document).ready(function(){
+
+    $('div.item').mouseenter(function(){
+
+        $(this).children('div.item_cover').css({'display':'block'}).animate({'opacity':'0.8'}, 'slow');
+
+    });
+
+    $('div.item').mouseleave(function(){
+
+        $(this).children('div.item_cover').stop().animate({'opacity':'0'}, 'slow', 'linear', function(){
+            $(this).css({'display':'none'});
+        });
+
+    });
+
+});
