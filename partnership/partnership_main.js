9ce83ac5730550c892c18deeeff45aa12650f285
gxlzlihao <gxlzlihao@gmail.com>
2016-04-18 14:23:42 +0800

    done with partnership page

--- partnership/partnership_main.js
+++ partnership/partnership_main.js
@@ -1,3 +1,14 @@
 /**
  * Created by gxlzlihao on 16/4/18.
  */
+
+$(document).ready(function(){
+
+    // reset the height of the night description section
+    var div_height = $('div.section#night_description').height();
+    var img_height = $('div.section#night_description').children('img.background_image').height();
+    if ( div_height != img_height ) {
+        $('div.section#night_description').children('img.background_image').css({ 'height':div_height });
+    }
+
+});
